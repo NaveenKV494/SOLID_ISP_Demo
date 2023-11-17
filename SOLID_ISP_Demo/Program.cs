2c79992165919e03178598170557f6fec3a350ac
TLSPC-079 <TLSPC-079@DESKTOP-AVJ9472>
2023-11-17 12:09:46 +0530

    Add project files.

---
+++ SOLID_ISP_Demo/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLID_ISP_Demo
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
